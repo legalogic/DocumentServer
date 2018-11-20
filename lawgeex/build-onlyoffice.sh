4247fa59563232424c0af9a73aadb250a5ecabb4
arik <arikkrol@gmail.com>
2018-11-20 11:30:52 +0200

    installing nodejs

--- lawgeex/build-onlyoffice.sh
+++ lawgeex/build-onlyoffice.sh
@@ -2,4 +2,17 @@
 
 
 echo 'building onlyofficeeeee'
-sleep infinity
+
+apt-get update
+apt-get upgrade -y
+apt-get install sudo
+sudo apt-get install -yq curl apt-transport-https ca-certificates
+
+#install
+sudo apt-get install gnupg2 -y
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sleep infinity #maybe change that to tail the onlyoffice log
