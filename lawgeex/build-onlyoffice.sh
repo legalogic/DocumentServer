b77f23dfaae5095af314a4f0ea1cea22caef78a0
arik <arikkrol@gmail.com>
2018-11-20 11:48:53 +0200

    installing grunt

--- lawgeex/build-onlyoffice.sh
+++ lawgeex/build-onlyoffice.sh
@@ -1,28 +1,21 @@
 
-
-
 echo 'building onlyofficeeeee'
 
 apt-get update
 apt-get upgrade -y
 apt-get install sudo
 
-#install nodejs
-# sudo apt-get install -yq curl apt-transport-https ca-certificates
-# sudo apt-get install gnupg2 -y
-# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-
-#install nodejs using nvm
+#install nodejs + npm using nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source ~/.bashrc
 nvm install 
 nvm install 8.12
 nvm use 8.12
+sudo npm install -g npm
 
+#install grunt
+sudo npm install -g grunt-cli
 
-#install npm
-#sudo apt-get install npm
 
 
 
