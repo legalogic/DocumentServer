49a583907e08f1a9194c7257ad7d3679759b845d
arik <arikkrol@gmail.com>
2018-11-20 12:03:36 +0200

    installing additional dependencies

--- lawgeex/build-onlyoffice.sh
+++ lawgeex/build-onlyoffice.sh
@@ -16,6 +16,25 @@ sudo npm install -g npm
 #install grunt
 sudo npm install -g grunt-cli
 
+#install additional dependencies
+sudo apt-get install --force-yes -yq \
+    wget \
+    build-essential \
+    libcurl4-gnutls-dev \
+    libglib2.0-dev \
+    libgdk-pixbuf2.0-dev \
+    libgtkglext1-dev \
+    libatk1.0-dev \
+    libcairo2-dev \
+    libxml2-dev \
+    libxss-dev \
+    libgconf2-dev \
+    default-jre \
+    qt5-qmake \
+    qt5-default  \
+    p7zip-full \
+    git \
+    subversion
 
 
 
