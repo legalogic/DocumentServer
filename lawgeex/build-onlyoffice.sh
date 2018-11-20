b4e3be0a25b0c42329ca4ff28a7c14e0785f3e83
arik <arikkrol@gmail.com>
2018-11-20 11:45:00 +0200

    installing node + npm with nvm

--- lawgeex/build-onlyoffice.sh
+++ lawgeex/build-onlyoffice.sh
@@ -6,13 +6,28 @@ echo 'building onlyofficeeeee'
 apt-get update
 apt-get upgrade -y
 apt-get install sudo
-sudo apt-get install -yq curl apt-transport-https ca-certificates
 
-#install
-sudo apt-get install gnupg2 -y
+#install nodejs
+# sudo apt-get install -yq curl apt-transport-https ca-certificates
+# sudo apt-get install gnupg2 -y
+# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+#install nodejs using nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.bashrc
+nvm install 
+nvm install 8.12
+nvm use 8.12
 
-sudo apt-get install -y nodejs
+
+#install npm
+#sudo apt-get install npm
+
+
+
+
+
+
 
 sleep infinity #maybe change that to tail the onlyoffice log
